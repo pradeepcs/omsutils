716f3de4e7b132894a47e5ac96866a914c5cc116
pradeepcs <cs.pradeep@gmail.com>
2016-06-02 07:15:48 +0530

    Changing implementation to Spring Integration

--- interopservice/src/main/java/org/csp/omsutils/interopservice/application/App.java
+++ interopservice/src/main/java/org/csp/omsutils/interopservice/application/App.java
@@ -1,37 +1,50 @@
 package org.csp.omsutils.interopservice.application;
 
-import java.util.Properties;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.csp.omsutils.interopservice.httpclient.InteropClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	
-	public static Properties prop;
-	
+
+	public static ConfigurableApplicationContext context = null;
+
 	static {
 		loadAppProperties();
 	}
-	
+
 	public static void loadAppProperties() {
-		try {
-			prop = new Properties();
-			prop.load(App.class.getClassLoader().getResourceAsStream("customer_override.properties"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
+		context = new ClassPathXmlApplicationContext("/spring/integration/http-outbound-config.xml");
 	}
-	
-    public static void main( String[] args ) throws Exception {
-        System.out.println( "Hello World!" );
-        getOrderDetails();
-    }
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello World!");
+		getOrderDetails();
+	}
 
 	private static void getOrderDetails() throws Exception {
-		System.out.println(InteropClient.invokeApiOrService("N", "getOrderDetails", "<Order OrderHeaderKey='2016051601473258807' />", "<Order OrderNo='' />"));
+
+		Map<String, String> requestMap = new HashMap<String, String>();
+//		requestMap.put("YFSEnvironment.progId", "SterlingHttpTester");
+//		requestMap.put("YFSEnvironment.userId", "admin");
+//		requestMap.put("YFSEnvironment.password", "password");
+
+		requestMap.put("InteropApiName", "getOrderDetails");
+		requestMap.put("IsFlow", "N");
+		requestMap.put("ApiName", "getOrderDetails");
+		requestMap.put("ServiceName", "getOrderDetails");
+		requestMap.put("InteropApiData", "<Order OrderHeaderKey='2016051601473258807' />");
+		requestMap.put("TemplateData", "<Order OrderNo='' EnterpriseCode='' DocumentType='' />");
+
+		InteropClient interopClient = context.getBean("requestGateway", InteropClient.class);
+		System.out.println(interopClient.invokeApiOrService(requestMap));
 	}
 
 }
--- interopservice/src/main/java/org/csp/omsutils/interopservice/httpclient/InteropClient.java
+++ interopservice/src/main/java/org/csp/omsutils/interopservice/httpclient/InteropClient.java
@@ -1,86 +1,9 @@
 package org.csp.omsutils.interopservice.httpclient;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.csp.omsutils.interopservice.application.App;
+public interface InteropClient {
 
-public class InteropClient {
-
-	
-	/**
-	 * 
-	 * @param isFlow
-	 * @param apiName
-	 * @param inDoc
-	 * @param template
-	 * @return
-	 * @throws Exception
-	 */
-	
-	public static String invokeApiOrService(String isFlow, String apiName, String inDoc, String template) throws Exception {
-		
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpPost post = InteropClient.getHttpPost(isFlow, apiName, inDoc, template);
-
-		HttpResponse response = client.execute(post);
-		System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
-
-		BufferedReader rd = new BufferedReader(
-		        new InputStreamReader(response.getEntity().getContent()));
-
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}	
-
-		System.out.println("Response output : " + result);
-
-		return result.toString();
-	}
-	
-	
-	/**
-	 * 
-	 * @param isFlow N/Y
-	 * @param apiName - API or Service Name
-	 * @param inDoc - API or Service input
-	 * @param template - API output template
-	 * @return
-	 * @throws UnsupportedEncodingException
-	 */
-
-	public static HttpPost getHttpPost(String isFlow, String apiName, String inDoc, String template) throws UnsupportedEncodingException {
-
-		HttpPost post = new HttpPost(App.prop.getProperty("interop_url"));
-		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-		urlParameters.add(new BasicNameValuePair("YFSEnvironment.progId", App.prop.getProperty("env_progId")));
-		urlParameters.add(new BasicNameValuePair("InteropApiName", apiName));
-		urlParameters.add(new BasicNameValuePair("IsFlow", isFlow));
-		if("N".equals(isFlow)) {
-			urlParameters.add(new BasicNameValuePair("ApiName", apiName));
-		} else {
-			urlParameters.add(new BasicNameValuePair("ServiceName", apiName));
-		}
-		urlParameters.add(new BasicNameValuePair("YFSEnvironment.userId", App.prop.getProperty("env_userId")));
-		urlParameters.add(new BasicNameValuePair("YFSEnvironment.password", App.prop.getProperty("env_password")));
-		urlParameters.add(new BasicNameValuePair("InteropApiData", inDoc));
-		if(template != null) {
-			urlParameters.add(new BasicNameValuePair("TemplateData", template));	
-		}
-		post.setEntity(new UrlEncodedFormEntity(urlParameters));
-		return post;
-	}
+	String invokeApiOrService(Map<String, String> request);
 
 }
